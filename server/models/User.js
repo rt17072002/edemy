cc980d564c8980561b4722421b3277b5794c5b80
Rajeev Tripathi <rt17072002@gmail.com>
2026-02-23 18:23:45 +0530

    Svix corrected

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    imageUrl: { type: String, default:"" },
     enrolledCourses: [
         {
             type: mongoose.Schema.Types.ObjectId,
